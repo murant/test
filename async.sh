b4cea44f10f3560908ed7b15711c85fa1902991d
murant <pltb1qed@s.okayama-u.ac.jp>
2016-09-16 13:53:18 +0900

    add async.sh

---
+++ async.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOMEDIR=/home/muraoka
+
+GITHOMEDIR=${HOMEDIR}/dk2-11.1
+
+cd ${GITHOMEDIR}
+
+USRNAME=kamou
+IPADDLR=172.21.48.8
+
+TARGETDIR=/home/muraoka/dk2-11.1
+TARGETSRC=${TARGETDIR}
+
+rsync -e "ssh -oHostKeyAlgorithms=+ssh-dss" -av ${GITHOMEDIR} muraoka@172.21.48.55:./
+ssh -oHostKeyAlgorithms=+ssh-dss muraoka@172.21.48.55 "cd ${TARGETDIR};sh make_all.sh"
